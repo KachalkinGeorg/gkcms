17549485da34676cb2472881e9ad19e270ecdc82
KachalkinGeorg <119577414+KachalkinGeorg@users.noreply.github.com>
2023-03-19 22:36:50 +0300

    lastcomments

--- engine/actions/lastcomments.php
+++ engine/actions/lastcomments.php
@@ -18,14 +18,47 @@ $breadcrumb = breadcrumb('<i class="fa fa-commenting-o btn-position"></i><span c
 
 if (!getPluginStatusActive('comments')) {
 	msg(['type' => 'error', 'text' => $lang['w_comm']]);
-	return print_msg( 'warning', $lang['lastcomments'], $lang['w_com'], '?mod=lastcomments' );
+	return print_msg( 'warning', $lang['lastcomments'], $lang['w_com'], 'javascript:history.go(-1)' );
 }
 
-if ($_REQUEST['action'] == 'commit') {
-	$perpage = $_REQUEST['perpage'];
-	$order = $_REQUEST['order'];
-	$comm_length = $_REQUEST['comm_length'];
+if ($_REQUEST['action'] == 'delete') {
+
+    $selected_com = getIsSet($_REQUEST['selected_com']);
+    if (!$selected_com || !is_array($selected_com)) {
+        msg(['type' => 'error', 'text' => $lang['msge_select']]);
+		print_msg( 'error', $lang['lastcomments'], $lang['msge_select'], '?mod=lastcomments' );
+
+        return;
+    }
+	
+    $db = NGEngine::getInstance()->getDB();
+    foreach ($selected_com as $query) {
+		
+		$db->exec("DELETE FROM ".prefix."_comments WHERE `id` = " . db_squote($query));
+		foreach ($mysql->select("select n.id, count(c.id) as cid from ".prefix."_news n left join ".prefix."_comments c on c.post=n.id group by n.id") as $row) {
+			$db->exec("update ".prefix."_news set com=".$row['cid']." where id = ".$row['id']);
+		}
+
+		// Обновляем счетчик постов у юзеров
+		foreach ($mysql->select("select author_id, count(*) as cnt from ".prefix."_news group by author_id") as $row) {
+			$db->exec("update ".uprefix."_users set news=".$row['cnt']." where id = ".$row['author_id']);
+		}
+	
+		foreach ($mysql->select("select n.id, count(c.id) as cid from ".prefix."_news n left join ".prefix."_comments c on c.post=n.id group by n.id") as $row) {
+			$db->exec("update ".prefix."_news set com=".$row['cid']." where id = ".$row['id']);
+		}
+
+		// Обновляем счетчик комментариев у юзеров
+		foreach ($mysql->select("select author_id, count(*) as cnt from ".prefix."_comments group by author_id") as $row) {
+			$db->exec("update ".uprefix."_users set com=".$row['cnt']." where id = ".$row['author_id']);
+		}
+    }
+	
+    msg(['type' => 'info', 'text' => $lang['msgo_deleted']]);
+	return print_msg( 'delete', $lang['lastcomments'], ''.$lang['msgo_deleted'].'', '?mod=lastcomments' );
+
 }
+
 	//$perpage = extra_get_param('show_comments', 'perpage');
 	$perpage = $_REQUEST['perpage'];
 	if ($perpage == '') {
@@ -45,6 +78,7 @@ if ($_REQUEST['action'] == 'commit') {
 	if (($comm_length < 10) || ($comm_length > 5000)) {
 		$comm_length = 50;
 	}
+	
 	// Выбираем из БД общее количество записей
 	$query = "SELECT COUNT(*) as cnt FROM " . prefix . "_comments";
 	$res = $mysql->record($query, 1);
@@ -64,12 +98,11 @@ if ($_REQUEST['action'] == 'commit') {
 	if ($start < 0) {
 		$start = 0;
 	}
+	
 	$query = "select c.id, c.postdate, c.author, c.author_id, c.mail, c.text, c.ip, n.id as nid, n.title, n.alt_name, n.catid, n.postdate as npostdate from " . prefix . "_comments c left join " . prefix . "_news n on c.post=n.id where n.approve=1 order by c.id " . $order . " limit " . $start . ", " . $perpage;
 	$result = $mysql->select($query);
-	// Выводим "шапку" таблицы
 
 	foreach ($result as $prd) {
-		// Parse comments
 		$text = $prd['text'];
 		if ($config['blocks_for_reg']) {
 			$text = $parse->userblocks($text);
@@ -97,117 +130,47 @@ if ($_REQUEST['action'] == 'commit') {
 
 		$data =  langdate('d.m.Y H:i:s', $prd['postdate']);
 		$com = '' . $text . '';
-		$news = '<a href="' . newsGenerateLink(array('id' => $prd['nid'], 'alt_name' => $prd['alt_name'], 'catid' => $prd['catid'], 'postdate' => $prd['npostdate'])) . '" target="_blank">' . str_replace('<', '&lt;', $prd['title']) . '</a> <a href="/engine/admin.php?mod=news&action=edit&id=' . $prd['nid'] . '" title="'.$lang['edit'].'" target="_blank"><i class="fa fa-pencil-square"></i></a>';
+		$news = '<a href="' . newsGenerateLink(array('id' => $prd['nid'], 'alt_name' => $prd['alt_name'], 'catid' => $prd['catid'], 'postdate' => $prd['npostdate'])) . '" title="'.$lang['open_news'].'" target="_blank">' . str_replace('<', '&lt;', $prd['title']) . '</a> <a href="?mod=news&action=edit&id=' . $prd['nid'] . '" title="'.$lang['edit'].'" target="_blank"><i class="fa fa-pencil-square"></i></a>';
 		if ($prd['author_id']) {
-			$author = '<a href="/engine/admin.php?mod=users&action=editForm&id=' . $prd['author_id'] . '" target="_blank">' . str_replace('<', '&lt;', $prd['author']) . '</a><br/><small><a href="mailto:' . $prd['mail'] . '">' . $prd['mail'] . '</a></small>';
+			$author = '<a href="?mod=users&action=editForm&id=' . $prd['author_id'] . '" title="'.$lang['edit'].'" target="_blank">' . str_replace('<', '&lt;', $prd['author']) . '</a><br/><small><a href="mailto:' . $prd['mail'] . '">' . $prd['mail'] . '</a></small>';
 		} else {
 			$author = '' . str_replace('<', '&lt;', $prd['author']) . '<br/><small><a href="mailto:' . $prd['mail'] . '">' . $prd['mail'] . '</a></small>';
 		}
-		$ip = '[' . $prd['ip'] . '] - <a href="/engine/admin.php?mod=ipban&iplock=' . $prd['ip'] . '" title="'.$lang['ipban'].'' . $prd['ip'] . '" target="_blank"><i class="fa fa-ban"></i></a> <a href="http://www.nic.ru/whois/?ip=' . $prd['ip'] . '" title="'.$lang['ipwhois'].'' . $prd['ip'] . '" target="_blank"><i class="fa fa-question-circle"></i></a>';
-		$check = '<input style="vertical-align: middle;" type="checkbox" name="type[]" value="' . $prd['id'] . '">';
-		$act = '<a style="vertical-align: middle;" href="/engine/admin.php?mod=editcomments&newsid=' . $prd['nid'] . '&comid=' . $prd['id'] . '" title="'.$lang['edit'].'" target="_blank"><i class="fa fa-pencil-square-o"></i></a> <a style="vertical-align: middle;" href="/engine/admin.php?mod=editcomments&subaction=deletecomment&newsid=' . $prd['nid'] . '&comid=' . $prd['id'] . '&poster=' . $prd['author'] . '" title="'.$lang['del'].'"><i class="fa fa-trash" style="color: red;"></i></a>';
+		$ip = '[' . $prd['ip'] . '] - <a href="?mod=ipban&iplock=' . $prd['ip'] . '" title="'.$lang['ipban'].'' . $prd['ip'] . '" target="_blank"><i class="fa fa-ban"></i></a> <a href="http://www.nic.ru/whois/?ip=' . $prd['ip'] . '" title="'.$lang['ipwhois'].'' . $prd['ip'] . '" target="_blank"><i class="fa fa-question-circle"></i></a>';
+		$act = '<a class="btn btn-outline-primary" href="?mod=editcomments&newsid=' . $prd['nid'] . '&comid=' . $prd['id'] . '" title="'.$lang['edit'].'" target="_blank"><i class="fa fa-pencil-square-o"></i></a> <a class="btn btn-outline-danger" href="?mod=editcomments&subaction=deletecomment&newsid=' . $prd['nid'] . '&comid=' . $prd['id'] . '&poster=' . $prd['author'] . '" title="'.$lang['del'].'"><i class="fa fa-trash" style="color: red;"></i></a>';
 
-        $tEntry = [
-            'date'   => $data,
+        $tEntry[] = [
+            'id'   	 => $prd['id'],
+			'date'   => $data,
 			'com'    => $com,
 			'news'   => $news,
 			'author' => $author,
 			'ip'     => $ip,
-			'check'  => $check,
 			'act'    => $act,
         ];
 		
-		$entries[] = $tEntry;
 	}
 
-	$type = $_POST['type'];
-	if (!empty($type)) {
-		// Начинаем формировать переменную, содержащую список
-		// в формате "(3,5,6,7)"
-		$query = "(";
-		foreach ($type as $val) $query .= "$val,";
-		// Удаляем последнюю запятую, заменяя её закрывающей скобкой )
-		$query = substr($query, 0, strlen($query) - 1) . ")";
-		// Завершаем формирование SQL-запроса на удаление
-		$query = "DELETE FROM " . prefix . "_comments WHERE id IN " . $query;
-		return print_msg( 'delete', $lang['lastcomments'], ''.$lang['com_del'].'', '?mod=lastcomments' );
-		// Выполняем запрос
-		if (!$mysql->query($query)) {
-			//$pages .=  "<br>" . $mysql->db_error() . "<br>";
-			//$pages .=  $query . "<br>";
-		} else {
-			foreach ($mysql->select("select n.id, count(c.id) as cid from " . prefix . "_news n left join " . prefix . "_comments c on c.post=n.id group by n.id") as $row) {
-				$mysql->query("update " . prefix . "_news set com=" . $row['cid'] . " where id = " . $row['id']);
-			}
-			// Обновляем счетчик постов у юзеров
-			foreach ($mysql->select("select author_id, count(*) as cnt from " . prefix . "_news group by author_id") as $row) {
-				$mysql->query("update " . uprefix . "_users set news=" . $row['cnt'] . " where id = " . $row['author_id']);
-			}
-			foreach ($mysql->select("select n.id, count(c.id) as cid from " . prefix . "_news n left join " . prefix . "_comments c on c.post=n.id group by n.id") as $row) {
-				$mysql->query("update " . prefix . "_news set com=" . $row['cid'] . " where id = " . $row['id']);
-			}
-			// Обновляем счетчик комментариев у юзеров
-			foreach ($mysql->select("select author_id, count(*) as cnt from " . prefix . "_comments group by author_id") as $row) {
-				$mysql->query("update " . uprefix . "_users set com=" . $row['cnt'] . " where id = " . $row['author_id']);
-			}
-			//$pages .=  "<META HTTP-EQUIV='Refresh' Content='0'>";
-		}
-	}
-	$uri = strtok(''.$_SERVER['REQUEST_URI'].'', "&") . "&";
-	if (count($_GET)) {
-		foreach ($_GET as $k => $v) {
-			if ($k != "page") $uri .= urlencode($k) . "=" . urlencode($v) . "&";
-		}
-	}
-	// Строим постраничную навигацию
-	if ($cnt_pages > 1) {
-		//   echo '<div style="margin:1em 0">&nbsp;Страницы:';
-		// Проверяем нужна ли стрелка "В начало"
-		if ($page > 3)
-			$startpage = '<li class="page-item"><a class="page-link" href="' . $uri . 'page=1">'.$lang['startpage'].'</a></li>';
-		else
-			$startpage = '';
-		// Проверяем нужна ли стрелка "В конец"
-		if ($page < ($cnt_pages - 2))
-			$endpage = '<li class="page-item"><a class="page-link" href="' . $uri . 'page=' . $cnt_pages . '">'.$lang['endpage'].'</a></li>';
-		else
-			$endpage = '';
-		// Находим две ближайшие станицы с обоих краев, если они есть
-		if ($page - 2 > 0)
-			$page2left = '<li class="page-item"><a class="page-link" href="' . $uri . 'page=' . ($page - 2) . '">' . ($page - 2) . '</a></li>';
-		else
-			$page2left = '';
-		if ($page - 1 > 0)
-			$page1left = '<li class="page-item"><a class="page-link" href="' . $uri . 'page=' . ($page - 1) . '">' . ($page - 1) . '</a></li>';
-		else
-			$page1left = '';
-		if ($page + 2 <= $cnt_pages)
-			$page2right = '<li class="page-item"><a class="page-link" href="' . $uri . 'page=' . ($page + 2) . '">' . ($page + 2) . '</a></li>';
-		else
-			$page2right = '';
-		if ($page + 1 <= $cnt_pages)
-			$page1right = '<li class="page-item"><a class="page-link" href="' . $uri . 'page=' . ($page + 1) . '">' . ($page + 1) . '</a></li>';
-		else
-			$page1right = '';
-		// Выводим меню
-		//$pages .=  '</div>';
-	}
-	$pages = '<li class="page-item">' . $startpage . $page2left . $page1left . '</li><li class="page-item active"><a class="page-link">' . $page . '</a></li><li class="page-item">' . $page1right . $page2right . $endpage . '</li>';
-
-	$del = '<input class="btn btn-danger" type="submit" value="'.$lang['del'].'">';
+    $pagesss = generateAdminPagelist([
+        'current' => $page,
+        'count'   => $cnt_pages,
+        'url'     => '?mod=lastcomments'.
+			(isset($_REQUEST['order']) && $_REQUEST['order'] ? '&order=' . $_REQUEST['order'] : '').
+			(isset($_REQUEST['perpage']) && $_REQUEST['perpage'] ? '&perpage='.intval($_REQUEST['perpage']) : '').
+			(isset($_REQUEST['comm_length']) && $_REQUEST['comm_length'] ? '&comm_length='.intval($_REQUEST['comm_length']) : '').
+            '&page=%page%',
+    ]);
+	
 	$orderes = '<option value="asc" ' . (empty($order) ? 'selected' : '') . '>'.$lang['order_asc'].'</option><option value="desc" ' . (!empty($order) ? 'selected' : '') . '>'.$lang['order_desc'].'</option>';
 		
-$tVars = [
-	'php_self' => $PHP_SELF,
-	'entries'   => $entries,
-	'perpage'   => $perpage,
-	'order'   => $orderes,
-	'comm_length'   => $comm_length,
-    'flags'   => [
-		'pages'  => $pages,
-    ],
-	'del' => $del,
-];
+	$tVars = [
+		'php_self' 	=> $PHP_SELF,
+		'entries' => isset($tEntry) ? $tEntry : '',
+		'perpage'   => $perpage,
+		'order'   	=> $orderes,
+		'comm_length'   => $comm_length,
+		'pagesss' => $pagesss,
+	];
 
 $xt = $twig->loadTemplate('skins/default/tpl/lastcomments.tpl');
 $main_admin = $xt->render($tVars);
--- engine/skins/default/tpl/lastcomments.tpl
+++ engine/skins/default/tpl/lastcomments.tpl
@@ -1,40 +1,55 @@
 <div id="collapseCommentFilter" class="collapse">
 	<div class="card mb-4">
+	<div class="card-header">{{ lang['filter'] }}</div>
 		<div class="card-body">
 			<form action="{{ php_self }}" method="get">
 			<input type="hidden" name="mod" value="lastcomments" />
 			<input type="hidden" name="action" value="list" />
 			
-			<table class="table table-sm">
+			<table class="table table-sm mb-0">
 				<tr>
-					<td width="50%">Кол-во комментариев для отображения на одной странице<small class="form-text text-muted">Значение по умолчанию: <b>5</b></small></td>
-					<td width="50%">
-						<input type="text" name="perpage" value="{{ perpage }}" class="form-control" />
+					<td><label style="padding: .375rem 0rem .75rem 0rem;">{{ lang['count_com'] }}</label></td>
+					<td>
+						<div class="input-group mb-3">
+							<div class="input-group-prepend input-group-append">
+								<label class="input-group-text"><i class="fa fa-scissors"></i></label>
+							</div>
+							<input type="number" name="comm_length" value="{{ comm_length }}" placeholder="{{ lang['length'] }}..." class="form-control" style="max-width:100px;text-align: center;"/>
+							<div class="input-group-prepend input-group-append">
+								<label class="input-group-text"><i class="fa fa-commenting"></i></label>
+							</div>
+						</div>
 					</td>
-				</tr>
-				<tr>
-					<td width="50%">Усечение длины комментария<small class="form-text text-muted">Значение по умолчанию: <b>50</b></small></td>
-					<td width="50%">
-						<input type="text" name="comm_length" value="{{ comm_length }}" class="form-control" />
+					<td><label style="padding: .375rem 0rem .75rem 0rem;">{{ lang['commet'] }}</label></td>
+					<td>
+						<div class="input-group mb-3">
+							<div class="input-group-prepend input-group-append">
+								<label class="input-group-text"><i class="fa fa-low-vision"></i></label>
+							</div>
+							<input type="number" name="perpage" value="{{ perpage }}" placeholder="{{ lang['commet'] }}..." class="form-control" style="max-width:150px;"/>
+						</div>
 					</td>
-				</tr>
-				<tr>
-					<td width="50%">Упорядочить по:<small class="form-text text-muted">Выберите порядок отображения комментариев.</small></td>
-					<td width="50%">
-						<select name="order" style="width: 22%;">{{ order }}</select>
+					<td><label style="padding: .375rem 0rem .75rem 0rem;">{{ lang['sort_by'] }}</label></td>
+					<td>
+						<div class="input-group mb-3">
+							<div class="input-group-prepend input-group-append">
+								<label class="input-group-text"><i class="fa fa-list-ol"></i></label>
+							</div>
+							<select name="order" style="width: 22%;">{{ order }}</select>
+						</div>
 					</td>
 				</tr>				
 			</table>
 
-						<div class="form-group mb-0 text-right">
-							<button type="submit" class="btn btn-outline-primary">{{ lang.sort }}</button>
-						</div>
+			<div class="panel-footer" align="center" style="margin-left: -20px;margin-right: -20px;margin-bottom: -20px;">
+				<button type="submit" class="btn btn-outline-primary">{{ lang['sort'] }}</button>
+			</div>
 			</form>
 		</div>
 	</div>
 </div>
 
-<form method="post" name="select_comments">
+<form action="?mod=lastcomments&action=delete" method="post" name="select_comments">
 <div class="panel panel-default">
   <div class="panel-heading">
  {{ lang.lastcomments }}
@@ -52,8 +67,9 @@
 						<th width="12%">{{ lang.date }}</th>
 						<th width="30%">{{ lang.com }}</th>
 						<th width="20%" nowrap>{{ lang.news }}</th>
-						<th width="14%">{{ lang.author }}</th>
-						<th width="14%">IP</th>
+						<th width="15%">{{ lang.author }}</th>
+						<th width="15%">IP</th>
+						<th width="10%" align="center">{{ lang.action }}</th>
 						<th width="5%" align="center">
 							<input type="checkbox" name="master_box" title="{{ lang.all_select }}" onclick="javascript:check_uncheck_all(select_comments)">
 						</th>
@@ -67,7 +83,16 @@
 						<td>{{ entry.news }}</td>
 						<td>{{ entry.author }}</td>
 						<td>{{ entry.ip }}</td>
-						<td style="vertical-align: middle;">{{ entry.check }} {{ entry.act }}</td>
+						<td style="vertical-align: middle;">
+							<div class="btn-group btn-group-sm" role="group">
+							{{ entry.act }}
+							</div>
+						</td>
+						<td style="vertical-align: middle;">
+							<div class="btn-group btn-group-sm" role="group">
+							<input style="vertical-align: middle;" type="checkbox" name="selected_com[]" value="{{ entry.id }}">
+							</div>
+						</td>
 					</tr>
 				{% else %}
 						<tr>
@@ -82,11 +107,11 @@
 	<div class="card-footer">
 		<div class="row">
 			<div class="col-lg-10 mb-2 mb-lg-0">
-			{% if flags.pages %}
-				<nav aria-label="Page navigation example"><ul class="pagination mb-0">{{ flags.pages }}</ul></nav>
+			{% if pagesss %}
+				<div class="col col-md-4">{{ pagesss }}</div>
 			{% endif %}
 			</div>
-			<div class="col-lg-2 text-right">{{ del }}</div>
+			<div class="col-lg-2 text-right"><input class="btn btn-danger" type="submit" value="{{ lang.del }}"></div>
 		</div>
 	</div>
 </div>
